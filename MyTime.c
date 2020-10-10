ab0181e7c7cd7ad74a2b9210cd5f870d6e05e96b
Nikita <valov.nl@phystech.edu>
2020-10-10 13:42:04 +0300

    Mytime.c count words, strings, bits

--- MyTime.c
+++ MyTime.c
@@ -13,29 +13,60 @@
 #include <fcntl.h>
 #include <string.h>
 
-int fd_stdout = 1;
-int fd_stdin = 2;
-int count = 1000;
+const int fd_stdout = 1;
+const int fd_stdin = 2;
+const int count = 1000;
 const int MAX_SIZE = 1000;
 
 
 struct timespec start, final;
 
+int my_read (int fd, char* buf, int len){
+
+    int bits = 0;
+    int last_read = -1;
+    int one_read_size = 10;
+
+    while (last_read != 0 ) {
+
+        last_read = read(fd, buf+bits, one_read_size);
+
+        if (last_read == -1) {
+            perror("My_read");
+            return -1;
+        }
+
+        bits = bits + last_read;
+
+        if (bits > len) {
+            printf("Size buf less than file!");
+            return -1;
+        }
+    }
+    return bits;
+}
+
+
 void count_words (char* buf, int count) {
     int sum = 0;
-
+    int strings = 0;
     for (int i = 0; i < count; i++ ) {
-
-        if (isspace(buf[i]) != 0)
+        if (isspace(buf[i]) != 0) {
+            if (buf[i] == '\n')
+                strings++;
             i++;
-
+        }
         else {
-            while (isspace(buf[i] == 0))
+            while (isspace(buf[i]) == 0) {
+                if (buf[i] == '\n')
+                    strings++;
                 i++;
+            }
             sum++;
         }
     }
     printf("Number of word is [%d]\n",sum);
+    printf("Number of string is [%d]\n", strings);
 }
 
 long int count_time (struct timespec start, struct timespec final){
@@ -56,15 +87,17 @@ long int count_time (struct timespec start, struct timespec final){
 
 int main (int argc, char** argv) {
     int pid = 0;
-    int status = 0;
-    int fd = 0;
     long bite_size = 0;
-    char buf [MAX_SIZE];
+    char* buf;
     int pipefd[2] = {0,0};
 
+    buf = calloc(MAX_SIZE, sizeof(char));
+
+    //Start time
     if(clock_gettime(CLOCK_REALTIME,&start) < 0)
         perror("Clock gettime start:");
 
+    //Create pipe
     if(pipe(pipefd) < 0)
         perror("Pipe is");
 
@@ -73,30 +106,43 @@ int main (int argc, char** argv) {
     // If i am child
     if (pid == 0) {
 
+        //Close read pipe
         if(close(pipefd[0]) < 0)
             perror("Pipefd close");
 
-        pipefd[1] = dup2(pipefd[1],1);
+        //Stdout of function is write pipe
+        pipefd[1] = dup2(pipefd[1],fd_stdout);
 
+        //Start function
         if(execvp(argv[1], argv + 1) < 0)
             perror("Exec is not working!");
+
         exit(0);
     }
 
+    //Wait child
     wait(NULL);
 
+    //Close pipe to write
     close(pipefd[1]);
 
-    bite_size = read(pipefd[0],&buf,count);
+    //Read from pipe
+    bite_size = my_read(pipefd[0],buf,count);
+
+    //Write to stdout
     write(fd_stdout,buf,bite_size);
 
+    //Print bits
     printf("\nBite_size is [%lu]\n",bite_size);
+
+    count_words(buf,bite_size);
 
     close(pipefd[0]);
 
+    //Stop time
     clock_gettime(CLOCK_REALTIME,&final);
 
-    //count_words(buf,bite_size);
+
     printf("Process is working [%.3d] ms\n", (int)(count_time(start,final)));
 
     return 0;
