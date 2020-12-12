13de5ac1f3cf6c19fbd7b5cf514352ba75061a4e
Nikita <valov.nl@phystech.edu>
2020-12-12 15:37:03 +0300

    Add depth

--- symlinks.c
+++ symlinks.c
@@ -17,26 +17,32 @@ static struct option long_opt[] = {
         {"name",0,0,'n'},
         {0,0,0,0}
 };
-int PATH_MAX = 1000;
 
-int find (char* path){
+int find (char* path, int flag){
     DIR* dir = opendir(path);
     struct dirent* entry = readdir(dir);
     struct stat* info;
     char pathName[PATH_MAX];
+
     if (dir == NULL)
         perror("Problem with open file!");
 
     while (entry != NULL){
-        (void)strncpy( pathName, theDir, PATH_MAX );
-        (void)strncat( pathName, "/", PATH_MAX );
-        (void)strncat( pathName, entry.d_name, PATH_MAX );
-        if (lstat(entry->d_name,info) < 0)
-            perror("Problem with stat");
-        if(S_ISDIR(info->st_mode) == 1)
-            printf("It is dir!\n");
 
-        printf("./%s\n",entry->d_name);
+        (void)strncpy( pathName, path, PATH_MAX );
+        (void)strncat( pathName, "/", 1024 );
+        (void)strncat( pathName, entry->d_name, PATH_MAX );
+
+        if (stat(pathName,info) < 0)
+            perror("Problem with stat");
+
+        if (flag == 1)
+            if (S_ISDIR(info->st_mode) == 1)
+                find(pathName,flag);
+
+        printf("%s \n", pathName);
+
+
         entry = readdir(dir);
     }
 
@@ -71,7 +77,7 @@ int main(int argc, char** argv) {
                 break;
             }
             default:
-                find(path);
+                find(path,flag);
                 return 0;
         }
     }
